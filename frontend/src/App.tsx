a75cf0ce21dcce1fe0c51e86fb6d6b90d10b6166
kkamara <kelvinkamara@protonmail.com>
2021-06-22 13:43:32 +0100

    Add app title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import { Helmet } from "react-helmet";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+          <title>Typescript React App</title>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
