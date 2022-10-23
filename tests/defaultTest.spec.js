1ae04ea053be7b0a9510c5a1689a3a458cdc9162
kkamara <kelvinkamara@protonmail.com>
2022-10-23 22:06:40 +0100

    Add google test I'm feeling lucky

--- tests/defaultTest.spec.js
+++ tests/defaultTest.spec.js
@@ -2,10 +2,12 @@ module.exports = {
   '@tags': ['beta'],
   'NW test on Google' : function (client) {
     client
+      .useXpath()
       .url('http://www.google.com')
-      .waitForElementVisible('body', 1000)
+      .click('//button[@id="L2AGLb"]')
       .assert.title('Google')
-      .pause(1000)
+      .click('//input[@id="gbqfbb"]')
+      .pause(5000)
       .end()
   }
 }
